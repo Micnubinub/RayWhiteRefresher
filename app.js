ecbad05ffb2c1da5b3e60ff04cc095a4b48c74fc
Micnubinub <lindelwencube.ln@gmail.com>
2015-10-13 07:31:03 +1000

    lodash

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -23,6 +23,7 @@ var tkn = "";
 var parentOfLoadedFile = "0ACAYq0kXNuoVUk9PVA";
 var loadedFileID = "0ByAYq0kXNuoVd2NvZ2R5dlMxeXM";
 
+console.log('were getting somewhere');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
