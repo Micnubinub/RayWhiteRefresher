1e39f0b7880d12794ad460a6032268b3171da6bb
Micnubinub <lindelwencube.ln@gmail.com>
2015-10-13 06:59:09 +1000

    comit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var fs = require('fs');
-var vm = require('vm');
+//var fs = require('fs');
+//var vm = require('vm');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
