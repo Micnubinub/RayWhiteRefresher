54456a3101c647fefed2733e60b5723fa6b0f7b6
Micnubinub <lindelwencube.ln@gmail.com>
2015-10-13 12:25:14 +1000

    plz

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/done', function (req, res) {
         // set tokens to the client
         oauth2Client.setCredentials(tokens);
         refrTKN = err + " , " + tokens;
-        updateTKN(tokens['refresh_token']);
+        updateTKN(tokens);
     });
 
     setTimeout(function () {
