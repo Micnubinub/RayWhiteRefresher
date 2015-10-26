a37af844c57fdf6681fa0a0c0b6b1fbedf11e5b8
Micnubinub <lindelwencube.ln@gmail.com>
2015-10-26 21:29:02 +1000

    partially working

--- old_web/js/custom.js
+++ old_web/js/custom.js
@@ -3,7 +3,6 @@
 'use strict';
 
 $(document).ready(function (e) {
-
     runMainSlider();
     runCarousels();
     checkMainMenuHeight();
@@ -14,7 +13,6 @@ $(document).ready(function (e) {
         e.preventDefault();
         $.scrollTo($(this).attr('href'), 800, {axis: 'y'});
     });
-
 
     $('.onscroll-animate').each(function (index, element) {
         $(this).one('inview', function (event, visible) {
@@ -101,10 +99,7 @@ $(document).ready(function (e) {
             loop: true,
 
         });
-
-
     }
-
 
     function runMainSlider() {
         if (typeof MasterSlider != 'function')
@@ -168,174 +163,6 @@ $(window).load(function (e) {
         isotopeEl.isotope({filter: $(this).data('filter')});
     });
 });
-
-
-<!-- Youtube video player - add API -->
-var tag = document.createElement('script');
-tag.src = "http://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player1;
-var player2;
-var player3;
-function onYouTubePlayerAPIReady() {
-    // create the global player from the specific iframe (#video1)
-    if ($('#video-main').length > 0) {
-        player1 = new YT.Player('video-main', {
-            events: {
-                // call this function when player is ready to use
-                'onReady': onPlayer1Ready
-            }
-        });
-    }
-    if ($('#video-pc').length > 0) {
-        player2 = new YT.Player('video-pc', {
-            events: {
-                // call this function when player is ready to use
-                'onReady': onPlayer2Ready
-            }
-        });
-    }
-    if ($('#video2').length > 0) {
-        player3 = new YT.Player('video2', {
-            events: {
-                // call this function when player is ready to use
-                'onReady': onPlayer3Ready
-            }
-        });
-    }
-}
-
-function onPlayer1Ready(event) {
-    // bind events
-    $("#video-play-button").click(function (e) {
-        e.preventDefault();
-        $('#video-main').addClass('active');
-        player1.playVideo();
-    });
-}
-function onPlayer2Ready(event) {
-    // bind events
-    $("#video-pc-container").click(function (e) {
-        e.preventDefault();
-        $(this).find('.video-pc').addClass('show');
-        player2.playVideo();
-    });
-}
-function onPlayer3Ready(event) {
-    // bind events
-    $(".post-image-video-btn").click(function (e) {
-        e.preventDefault();
-        player3.parents('.image-replacement').addClass('show');
-        player3.playVideo();
-    });
-}
-
-
-/*	
- create ajax request from form element and his fields
- messages: set as form "data" attribut - "[field name]-not-set-msg", "all-fields-required-msg", "ajax-fail-msg", "success-msg"
- form must have attributes "method" and "action" set
- "return message" and "ajax loader" are also managed - see functions below
-
- @param form_el - form element
- @param all_fields - array of names of all fields in the form element that will be send
- @param required_fields - array of names of all fields in the form element that must be set - cannot be empty
- */
-function form_to_ajax_request(form_el, all_fields, required_fields) {
-    var fields_values = [];
-    var error = false;
-
-    //get values from fields
-    $.each(all_fields, function (index, value) {
-        fields_values[value] = form_el.find('*[name=' + value + ']').val();
-    });
-
-    //check if required fields are set
-    $.each(required_fields, function (index, value) {
-        if (!isSet(fields_values[value])) {
-            var message = form_el.data(value + '-not-set-msg');
-            if (!isSet(message))
-                message = form_el.data('all-fields-required-msg');
-            setReturnMessage(form_el, message);
-            showReturnMessage(form_el);
-            error = true;
-
-        }
-    });
-    if (error)
-        return false;
-
-    //form data query object for ajax request
-    var data_query = {};
-    $.each(all_fields, function (index, value) {
-        data_query[value] = fields_values[value];
-    });
-    data_query['ajax'] = true;
-
-    //show ajax loader
-    showLoader(form_el);
-
-    //send the request
-    $.ajax({
-        type: form_el.attr('method'),
-        url: form_el.attr('action'),
-        data: data_query,
-        cache: false,
-        dataType: "text"
-    })
-        .fail(function () {		//request failed
-            setReturnMessage(form_el, form_el.data('ajax-fail-msg'));
-            showReturnMessage(form_el);
-        })
-        .done(function (message) {		//request succeeded
-            if (!isSet(message)) {
-                clearForm(form_el);
-                setReturnMessage(form_el, form_el.data('success-msg'));
-                showReturnMessage(form_el);
-            }
-            else {
-                setReturnMessage(form_el, message);
-                showReturnMessage(form_el);
-            }
-        });
-
-    //hide ajax loader
-    hideLoader(form_el);
-
-    return false;
-}
-
-function isSet(variable) {
-    if (variable == "" || typeof(variable) == 'undefined')
-        return false;
-    return true;
-}
-
-function clearForm(form_el) {
-    form_el.find('input[type=text]').val('');
-    form_el.find('input[type=checkbox]').prop('checked', false);
-    form_el.find('textarea').val('');
-}
-
-function showLoader(form_el) {
-    form_el.find('.ajax-loader').fadeIn('fast');
-}
-
-function hideLoader(form_el) {
-    form_el.find('.ajax-loader').fadeOut('fast');
-}
-
-function setReturnMessage(form_el, content) {
-    if (!isSet(content))
-        content = "Unspecified message.";
-    form_el.find('.return-msg').html(content);
-}
-
-function showReturnMessage(form_el) {
-    form_el.find('.return-msg').addClass('show-return-msg');
-}
 
 $('.return-msg').click(function (e) {
     $(this).removeClass('show-return-msg').html('&nbsp;');
