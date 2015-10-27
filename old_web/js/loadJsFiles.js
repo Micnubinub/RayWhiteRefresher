333df543ad38c850052ffd1ffaca557780fef5c5
Micnubinub <lindelwencube.ln@gmail.com>
2015-10-27 10:45:18 +1000

    partially working

--- old_web/js/loadJsFiles.js
+++ old_web/js/loadJsFiles.js
@@ -65,7 +65,6 @@ function loadJSFile(req) {
         }
         loadCSSFiles();
     };
-
     xmlHttp.send();
 }
 
@@ -91,6 +90,5 @@ function loadCSSFile(req) {
 
         loadCSSFiles();
     };
-
     xmlHttp.send();
 }
